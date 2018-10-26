d8e66e80cb717387cb14ea87c497619094fb1363
穆国超 <muguochao@qijiayoudao.com>
2018-10-26 17:06:23 +0800

    spider

--- src/Music_comment1.py
+++ src/Music_comment1.py
@@ -4,14 +4,17 @@ import requests
 import json
 
 headers = {
-    'Cookie' : 'appver=1.5.0.75771;',
-    'Referer' : 'http://music.163.com/'
+    'Cookie': 'appver=1.5.0.75771;',
+    'Referer': 'http://music.163.com/',
+    'Accept-Language': "zh-CN,zh;q=0.9,en;q=0.8",
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36',
 }
 
 first_param = "{rid:\"\",offset:\"0\",total:\"true\",limit:\"20\",csrf_tok:\"\"}"
 second_param = "010001"
 third_param = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7"
 fourth_param = "0CoJUm6Qyw8W8jud"
+
 
 def get_encSecKey():
     encSecKey = "257348aecb5e556c066de214e531faadd1c55d814f9be95fd06d6bff9f4c7a41f831f6394d5a3fd2e3881736d94a02ca919d952872e7d0a50ebfa1769a7a62d512f5f1ca21aec60bc3819a9c3ffca5eca9a0dba6d6f7249b06f5965ecfff3695b54e1c28f3f624750ed39e7de08fc8493242e26dbc4484a01c76f739e135637c"
@@ -36,29 +39,32 @@ def AES_encrypt(text, key, iv):
     return encrypt_text
 '''
 
+
 def get_params():
     iv = "0102030405060708"
     first_key = fourth_param
     second_key = 16 * 'F'
-    h_encText = AES_encrypt(first_param,first_key,iv)
-    h_encText = AES_encrypt(h_encText,second_key,iv)
+    h_encText = AES_encrypt(first_param, first_key, iv)
+    h_encText = AES_encrypt(h_encText, second_key, iv)
     return h_encText
 
 
-def get_Json(url,params,encSecKey):
+def get_Json(url, params, encSecKey):
     data = {
-        "params":params,
-        "encSecKey":encSecKey
+        "params": params,
+        "encSecKey": encSecKey
     }
-    response = requests.post(url,headers=headers,data=data)
+    response = requests.post(url, headers=headers, data=data)
     return response.content
+
 
 if __name__ == "__main__":
     url = "http://music.163.com/weapi/v1/resource/comments/A_PL_0_987409697?csrf_token="
     params = get_params()
     encSecKey = get_encSecKey()
-    json_text = get_Json(url,params,encSecKey)
-    json_dict = json.load(json_text)
+    json_text = get_Json(url, params, encSecKey)
+    json_dict = json.loads(json_text)
+    # print(json_dict)
     print(json_dict['total'])
     for item in json_dict['comments']:
         print(item['content'])
